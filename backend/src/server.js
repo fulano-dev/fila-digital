f1048006ce64164ace7a3c27cd90013ce6d5a445
Joao Pedro <joao@fulano.dev>
2025-04-30 20:59:29 -0300

    Revert "wewe"
    This reverts commit 35589714e44454c3c41061b2cc6ac2b6913f8439.

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,18 +7,18 @@ const filaRoutes = require('./routes/filaRoutes');
 const clienteRoutes = require('./routes/clienteRoutes');
 const restaurantRoutes = require('./routes/restaurantRoutes');
 const layoutRoutes = require('./routes/layoutRoutes');
+const WebSocket = require('ws'); // Importando o WebSocket
 
 // Verificar as variáveis de ambiente
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+console.log('DB_NAME:', process.env.DB_NAME);
 
 const app = express();
 
-// Configuração do CORS - Permitir todas as origens
-app.use(cors({
-  origin: '*', // Permite apenas o frontend
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
-  credentials: true, // Permite cookies, se necessário
-}));
-
+// Configuração do CORS
+app.use(cors());
 app.use(bodyParser.json()); // Parse JSON bodies
 
 // Configuração do banco de dados MySQL
@@ -45,6 +45,29 @@ app.use('/api/restaurants', restaurantRoutes);
 app.use(layoutRoutes);
 
 // Criar o servidor HTTP
-const server = app.listen(5000, '0.0.0.0', () => {
-    console.log(`Servidor rodando na porta ${process.env.PORT || 5000}`);
+const server = app.listen(process.env.PORT || 3001, '0.0.0.0', () => {
+    console.log(`Servidor rodando na porta ${process.env.PORT || 3001}`);
+});
+
+// Criar o servidor WebSocket e associar ao servidor HTTP
+const wss = new WebSocket.Server({ noServer: true }); // Alterando para não criar um novo servidor
+
+wss.on('connection', (ws) => {
+    console.log('Novo cliente conectado ao WebSocket');
+
+    // Aqui você pode tratar a comunicação via WebSocket, por exemplo, ouvindo mensagens
+    ws.on('message', (message) => {
+        console.log('Mensagem recebida:', message);
+    });
+
+    ws.on('close', () => {
+        console.log('Cliente desconectado');
+    });
+});
+
+// Adicionando a função de upgrade de WebSocket ao servidor HTTP
+server.on('upgrade', (request, socket, head) => {
+    wss.handleUpgrade(request, socket, head, (ws) => {
+        wss.emit('connection', ws, request);
+    });
 });
